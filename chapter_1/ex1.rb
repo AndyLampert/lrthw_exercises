4200dde2493a4f9daf36cfbf2494fa31bf451161
Andy Lampert <lamperta@gmail.com>
2015-06-02 21:32:52 -0600

    fix outstanding Guard offenses
    [#95932408]

--- chapter_1/ex1.rb
+++ chapter_1/ex1.rb
@@ -7,8 +7,9 @@ puts 'I\'d much rather you \'not\'.'
 puts 'I "said" do not touch this.'
 
 # Study Drill exercises
-puts "=========== Study Drills ============="
-puts "\n"
-# For "Make your script print only one of the lines.", I highlighted all but one line and hit command-/ on my
+puts 'Study Drills'
+puts '\n'
+# For "Make your script print only one of the lines.",
+# I highlighted all but one line and hit command-/ on my
 # keyboard to comment them out.
 # The octothorpe is how you add comments in Ruby.
