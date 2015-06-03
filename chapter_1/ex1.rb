03037b5359feabd1b3ffc4a08cbe69eccfea3da9
Andy Lampert <lamperta@gmail.com>
2015-06-02 21:03:55 -0600

    add study drill questions and update readme
    [#95932408]

--- chapter_1/ex1.rb
+++ chapter_1/ex1.rb
@@ -5,3 +5,10 @@ puts 'This is fun.'
 puts 'Yay! Printing.'
 puts 'I\'d much rather you \'not\'.'
 puts 'I "said" do not touch this.'
+
+# Study Drill exercises
+puts "=========== Study Drills ============="
+puts "\n"
+# For "Make your script print only one of the lines.", I highlighted all but one line and hit command-/ on my
+# keyboard to comment them out.
+# The octothorpe is how you add comments in Ruby.
