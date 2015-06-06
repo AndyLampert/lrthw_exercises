caa2d5f7756575aba78e651408030eb7a79f912f
Andy Lampert <lamperta@gmail.com>
2015-06-06 16:10:45 -0600

    complete and chapter 6
    [#95932418]

---
+++ chapter_6/Readme.md
@@ -0,0 +1,4 @@
+### Study Drills
+
+2. Find all the places where a string is put inside a string. There are four places.
+
---
+++ chapter_6/ex6.rb
@@ -0,0 +1,38 @@
+# Set the variable to a number
+types_of_people = 10
+# Set the variable to a string with the computed value
+x = "There are #{types_of_people} types of people."
+# Set the variable to a string
+binary = 'binary'
+# Set the variable to a string
+do_not = 'don\'t'
+
+# Set the variable to a string with the 2 computed values
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Print the string value stored in the variable
+puts x
+# Print the string value stored in the variable
+puts y
+
+# Print the string with the computed value
+puts "I said: #{x}."
+# Print the string with the computed value
+puts "I also said: '#{y}'."
+
+# Set the variable to a boolean
+hilarious = false
+
+# Set the variable to a string with the computed value
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Print the string value of the variable
+puts joke_evaluation
+
+# Set the variable to a string
+w = 'This is the left side of...'
+# Set the variable to a string
+e = 'a string with a right side.'
+
+# Print the string value of the two variables concatenated together
+puts w + e
